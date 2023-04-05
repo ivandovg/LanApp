50ed6e2e6581c3bfafbf7584156764231e1bdc6b
dovgoluckij_i@itstep.academy <dovgoluckij_i@itstep.academy>
2023-04-05 19:31:01 +0300

    Lesson 8-1

---
+++ LanApp8_1/FormHttpClient.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace LanApp8_1
+{
+    partial class FormHttpClient
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.edHttpContent = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbHttpHeaders = new System.Windows.Forms.ListBox();
+            this.btnHttpLoad = new System.Windows.Forms.Button();
+            this.edHttpAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.edHttpContent);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.lbHttpHeaders);
+            this.groupBox2.Controls.Add(this.btnHttpLoad);
+            this.groupBox2.Controls.Add(this.edHttpAddress);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(503, 426);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "HTTP Test";
+            // 
+            // edHttpContent
+            // 
+            this.edHttpContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.edHttpContent.Location = new System.Drawing.Point(6, 249);
+            this.edHttpContent.Multiline = true;
+            this.edHttpContent.Name = "edHttpContent";
+            this.edHttpContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.edHttpContent.Size = new System.Drawing.Size(491, 171);
+            this.edHttpContent.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Content";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Headers";
+            // 
+            // lbHttpHeaders
+            // 
+            this.lbHttpHeaders.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbHttpHeaders.FormattingEnabled = true;
+            this.lbHttpHeaders.Location = new System.Drawing.Point(6, 78);
+            this.lbHttpHeaders.Name = "lbHttpHeaders";
+            this.lbHttpHeaders.Size = new System.Drawing.Size(491, 134);
+            this.lbHttpHeaders.TabIndex = 9;
+            // 
+            // btnHttpLoad
+            // 
+            this.btnHttpLoad.Location = new System.Drawing.Point(402, 22);
+            this.btnHttpLoad.Name = "btnHttpLoad";
+            this.btnHttpLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnHttpLoad.TabIndex = 8;
+            this.btnHttpLoad.Text = "Load";
+            this.btnHttpLoad.UseVisualStyleBackColor = true;
+            this.btnHttpLoad.Click += new System.EventHandler(this.btnHttpLoad_Click);
+            // 
+            // edHttpAddress
+            // 
+            this.edHttpAddress.Location = new System.Drawing.Point(65, 24);
+            this.edHttpAddress.Name = "edHttpAddress";
+            this.edHttpAddress.Size = new System.Drawing.Size(331, 20);
+            this.edHttpAddress.TabIndex = 7;
+            this.edHttpAddress.Text = "https://www.google.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Address";
+            // 
+            // FormHttpClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "FormHttpClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HttpClient test";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox edHttpContent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbHttpHeaders;
+        private System.Windows.Forms.Button btnHttpLoad;
+        private System.Windows.Forms.TextBox edHttpAddress;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LanApp8_1/FormHttpClient.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net;
+
+namespace LanApp8_1
+{
+    public partial class FormHttpClient : Form
+    {
+        // клиент для обработки запорсов к серверу по протоколу Http
+        private static HttpClient httpClient = new HttpClient();
+        public FormHttpClient()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnHttpLoad_Click(object sender, EventArgs e)
+        {
+            lbHttpHeaders.Items.Clear();
+
+            Uri uri = null;
+            try
+            {
+                uri = new Uri(edHttpAddress.Text);
+            }
+            catch
+            {
+                MessageBox.Show("error address!");
+                return;
+            }
+
+            // для получения cookie, создаем контейнер
+            CookieContainer cookieContainer = new CookieContainer();
+
+            // формирование запроса к серверу
+            using (HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, uri))
+            {
+                // добавляем заголовки, при необходимости
+                //httpRequestMessage.Headers.Add()
+                httpRequestMessage.Headers.Add("User-Agent", "Chrome/51.0.2704.103 Safari/537.36 (Windows NT 6.1; Win64; x64; rv:47.0)");
+
+                // отправка запроса
+                using (HttpResponseMessage httpResponse = await httpClient.SendAsync(httpRequestMessage))
+                {
+                    lbHttpHeaders.Items.Add($"IsSuccessStatusCode: {httpResponse.IsSuccessStatusCode}");
+                    lbHttpHeaders.Items.Add($"StatusCode: {httpResponse.StatusCode}");
+                    lbHttpHeaders.Items.Add($"ReasonPhrase: {httpResponse.ReasonPhrase}");
+                    lbHttpHeaders.Items.Add("");
+                    lbHttpHeaders.Items.Add("Headers: ");
+
+                    // обработка заголовков
+                    foreach (var header in httpResponse.Headers)
+                    {
+                        lbHttpHeaders.Items.Add(header.Key);
+                        foreach (var headerValue in header.Value)
+                        {
+                            lbHttpHeaders.Items.Add(headerValue);
+                        }
+                        lbHttpHeaders.Items.Add("------------------------------------");
+                    }
+
+                    // заполнить контейнер cookie
+                    foreach (var cookieHeader in httpResponse.Headers.GetValues("Set-Cookie"))
+                    {
+                        cookieContainer.SetCookies(uri, cookieHeader);
+                    }
+
+                    // отобразить cookie
+                    lbHttpHeaders.Items.Add("");
+                    lbHttpHeaders.Items.Add("Cookie: ");
+                    foreach (Cookie cookie in cookieContainer.GetCookies(uri))
+                    {
+                        lbHttpHeaders.Items.Add($"{cookie.Name}: {cookie.Value}");
+                        lbHttpHeaders.Items.Add($"Cookie Domain: {cookie.Domain}");
+                    }
+
+                    // отображаем контент
+                    edHttpContent.Text = await httpResponse.Content.ReadAsStringAsync();
+                }
+            }
+        }
+    }
+}
